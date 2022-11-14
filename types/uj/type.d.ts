585683f0d4cb7328821e3cb0da88dc922652c8b7
Kirill <kirill-782@yandex.ru>
2022-11-14 17:19:25 +0300

    Update to UJ 1.0.5

--- types/uj/type.d.ts
+++ types/uj/type.d.ts
@@ -1,55 +1,51 @@
 /** @noSelfInFile **/
 
-declare class HDefenseType extends HHandle {
-  private defensetype: unknown;
+declare class HUnitWeaponBooleanField extends HHandle {
+  private unitweaponbooleanfield: unknown;
   protected constructor();
 }
-declare class HTextAlignType extends HHandle {
-  private textaligntype: unknown;
-  protected constructor();
-}
-declare class HRegenType extends HHandle {
-  private regentype: unknown;
+declare class HAbilityBooleanLevelField extends HHandle {
+  private abilitybooleanlevelfield: unknown;
   protected constructor();
 }
 declare class HOriginFrameType extends HHandle {
   private originframetype: unknown;
   protected constructor();
 }
-declare class HSubAnimType extends HHandle {
-  private subanimtype: unknown;
+declare class HAbilityStringField extends HHandle {
+  private abilitystringfield: unknown;
   protected constructor();
 }
 declare class HFrameEventType extends HHandle {
   private frameeventtype: unknown;
   protected constructor();
 }
-declare class HAnimType extends HHandle {
-  private animtype: unknown;
+declare class HAbilityStringLevelArrayField extends HHandle {
+  private abilitystringlevelarrayfield: unknown;
   protected constructor();
 }
-declare class HHeroAttribute extends HHandle {
-  private heroattribute: unknown;
-  protected constructor();
-}
-declare class HFrameHandle extends HHandle {
-  private framehandle: unknown;
+declare class HItemRealField extends HHandle {
+  private itemrealfield: unknown;
   protected constructor();
 }
 declare class HFramePointType extends HHandle {
   private framepointtype: unknown;
   protected constructor();
 }
-declare class HOsKeyType extends HHandle {
-  private oskeytype: unknown;
-  protected constructor();
-}
-declare class HTargetFlag extends HHandle {
-  private targetflag: unknown;
+declare class HAbilityIntegerField extends HHandle {
+  private abilityintegerfield: unknown;
   protected constructor();
 }
 declare class HPathingFlag extends HHandle {
   private pathingflag: unknown;
+  protected constructor();
+}
+declare class HAbilityIntegerLevelField extends HHandle {
+  private abilityintegerlevelfield: unknown;
+  protected constructor();
+}
+declare class HUnitBooleanField extends HHandle {
+  private unitbooleanfield: unknown;
   protected constructor();
 }
 declare class HUnitCategory extends HHandle {
@@ -62,5 +58,109 @@ declare class HArmorType extends HHandle {
 }
 declare class HMoveType extends HHandle {
   private movetype: unknown;
+  protected constructor();
+}
+declare class HDefenseType extends HHandle {
+  private defensetype: unknown;
+  protected constructor();
+}
+declare class HTextAlignType extends HHandle {
+  private textaligntype: unknown;
+  protected constructor();
+}
+declare class HItemBooleanField extends HHandle {
+  private itembooleanfield: unknown;
+  protected constructor();
+}
+declare class HRegenType extends HHandle {
+  private regentype: unknown;
+  protected constructor();
+}
+declare class HSubAnimType extends HHandle {
+  private subanimtype: unknown;
+  protected constructor();
+}
+declare class HAnimType extends HHandle {
+  private animtype: unknown;
+  protected constructor();
+}
+declare class HHeroAttribute extends HHandle {
+  private heroattribute: unknown;
+  protected constructor();
+}
+declare class HAbilityIntegerLevelArrayField extends HHandle {
+  private abilityintegerlevelarrayfield: unknown;
+  protected constructor();
+}
+declare class HUnitRealField extends HHandle {
+  private unitrealfield: unknown;
+  protected constructor();
+}
+declare class HFrameHandle extends HHandle {
+  private framehandle: unknown;
+  protected constructor();
+}
+declare class HUnitStringField extends HHandle {
+  private unitstringfield: unknown;
+  protected constructor();
+}
+declare class HAbilityRealField extends HHandle {
+  private abilityrealfield: unknown;
+  protected constructor();
+}
+declare class HUnitWeaponStringField extends HHandle {
+  private unitweaponstringfield: unknown;
+  protected constructor();
+}
+declare class HUnitWeaponRealField extends HHandle {
+  private unitweaponrealfield: unknown;
+  protected constructor();
+}
+declare class HOsKeyType extends HHandle {
+  private oskeytype: unknown;
+  protected constructor();
+}
+declare class HTargetFlag extends HHandle {
+  private targetflag: unknown;
+  protected constructor();
+}
+declare class HUnitWeaponIntegerField extends HHandle {
+  private unitweaponintegerfield: unknown;
+  protected constructor();
+}
+declare class HUnitIntegerField extends HHandle {
+  private unitintegerfield: unknown;
+  protected constructor();
+}
+declare class HItemIntegerField extends HHandle {
+  private itemintegerfield: unknown;
+  protected constructor();
+}
+declare class HAbilityRealLevelField extends HHandle {
+  private abilityreallevelfield: unknown;
+  protected constructor();
+}
+declare class HAbilityStringLevelField extends HHandle {
+  private abilitystringlevelfield: unknown;
+  protected constructor();
+}
+declare class HTimeType extends HHandle {
+  private timetype: unknown;
+  protected constructor();
+}
+declare class HItemStringField extends HHandle {
+  private itemstringfield: unknown;
+  protected constructor();
+}
+declare class HAbilityBooleanField extends HHandle {
+  private abilitybooleanfield: unknown;
+  protected constructor();
+}
+declare class HAbilityBooleanLevelArrayField extends HHandle {
+  private abilitybooleanlevelarrayfield: unknown;
+  protected constructor();
+}
+declare class HAbilityRealLevelArrayField extends HHandle {
+  private abilityreallevelarrayfield: unknown;
   protected constructor();
 }
